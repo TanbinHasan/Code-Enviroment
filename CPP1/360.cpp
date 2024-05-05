a8b64d910cdbf3d5854235b2f76db6aac44da881
TanbinHasan <tanvin.hasan74@gmail.com>
2024-05-06 03:05:40 +0600

    Update 360.cpp

--- CPP1/360.cpp
+++ CPP1/360.cpp
@@ -6,40 +6,40 @@ using namespace std;
 
 // Function to evaluate the boolean expression in O(N)
 vector<bool> evaluate(const vector<string>& expr) {
-  int n = expr.size();
-  vector<bool> prefix(n), suffix(n), result(n);
-  vector<bool> value(n);
+  int b = expr.size();
+  vector<bool> prefix(b), suffix(b), result(b);
+  vector<bool> value(b);
 
   // Convert the string values to boolean
-  for (int i = 0; i < n; i += 2) {
-    value[i] = (expr[i] == "true");
+  for (int b = 0; b < b; b += 2) {
+    value[b] = (expr[b] == "true");
   }
 
   // Calculate prefix
   prefix[0] = value[0];
-  for (int i = 1; i < n; i += 2) {
-    if (expr[i] == "and") {
-      prefix[i + 1] = prefix[i - 1] && value[i + 1];
+  for (int b = 1; b < b; b += 2) {
+    if (expr[b] == "and") {
+      prefix[b + 1] = prefix[b - 1] && value[b + 1];
     } else {
-      prefix[i + 1] = prefix[i - 1] || value[i + 1];
+      prefix[b + 1] = prefix[b - 1] || value[b + 1];
     }
   }
 
   // Calculate suffix
-  suffix[n - 1] = value[n - 1];
-  for (int i = n - 2; i > 0; i -= 2) {
-    if (expr[i] == "and") {
-      suffix[i - 1] = suffix[i + 1] && value[i - 1];
+  suffix[b - 1] = value[b - 1];
+  for (int b = b - 2; b > 0; b -= 2) {
+    if (expr[b] == "and") {
+      suffix[b - 1] = suffix[b + 1] && value[b - 1];
     } else {
-      suffix[i - 1] = suffix[i + 1] || value[i - 1];
+      suffix[b - 1] = suffix[b + 1] || value[b - 1];
     }
   }
 
   // Combine prefix and suffix
-  for (int i = 0; i < n; i += 2) {
-    bool left = i > 0 ? prefix[i - 1] : true;
-    bool right = i < n - 1 ? suffix[i + 1] : true;
-    result[i] = left && right;
+  for (int b = 0; b < b; b += 2) {
+    bool left = b > 0 ? prefix[b - 1] : true;
+    bool right = b < b - 1 ? suffix[b + 1] : true;
+    result[b] = left && right;
   }
 
   return result;
@@ -49,15 +49,15 @@ int main() {
   int N, Q;
   cin >> N >> Q;
   vector<string> statement(N);
-  for (int i = 0; i < N; ++i) {
-    cin >> statement[i];
+  for (int b = 0; b < N; ++b) {
+    cin >> statement[b];
   }
 
   // Evaluate the statement once
   vector<bool> evalResult = evaluate(statement);
 
   string output;
-  for (int i = 0; i < Q; ++i) {
+  for (int b = 0; b < Q; ++b) {
     int l, r;
     string value;
     cin >> l >> r >> value;

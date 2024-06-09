b11e03b30ebbf1076a7619d362d2c1cf6cc0492f
TanbinHasan <tanvin.hasan74@gmail.com>
2024-06-10 03:04:23 +0600

    'commit'

--- CPP1/A.cpp
+++ CPP1/A.cpp
@@ -1,116 +1,80 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const int MX = (int)2e5 + 1;  // n + 1
-const int K = 3;              // K-Hashing
-using H = array<int, K>;
-using HH = array<H, 2>;
-const H MOD = {127657753, 987654319, 1000000007};  // use K moduli
-const H B = {137, 277, 37};                // use K bases
-H P[MX], IP[MX];                       // power of base and inverse bases
-bool ISCALC;
+typedef long long i64;
 
-H operator+(H a, H b) {
-  H res;
-  for (int i = 0; i < K; ++i) res[i] = (a[i] + b[i]) % MOD[i];
-  return res;
-}
-H operator-(H a, H b) {
-  H res;
-  for (int i = 0; i < K; ++i) res[i] = (a[i] - b[i] + MOD[i]) % MOD[i];
-  return res;
-}
-H operator*(H a, H b) {
-  H res;
-  for (int i = 0; i < K; ++i) res[i] = (1LL * a[i] * b[i]) % MOD[i];
-  return res;
-}
-H operator*(H a, int b) {
-  H res;
-  for (int i = 0; i < K; ++i) res[i] = (1LL * a[i] * b) % MOD[i];
-  return res;
-}
-HH operator+(HH a, HH b) { return {a[0] + b[0], a[1] + b[1]}; }
-
-int power(int b, int p, int mod) {
-  b %= mod;
-  if (b < 0) b += mod;
-  int r = 1;
-  while (p) {
-    if (p & 1) r = (1LL * (r % mod) * (b % mod)) % mod;
-    b = (1LL * (b % mod) * (b % mod)) % mod;
-    p >>= 1;
-  }
-  return r;
-}
-
-class Hashing {
-  bool BasePowerCalculation(void) {
-    H IB;
-    for (int i = 0; i < K; ++i) IB[i] = power(B[i], MOD[i] - 2, MOD[i]);
-    P[0] = IP[0] = {1, 1};
-    for (int i = 1; i < MX; ++i) {
-      P[i] = P[i - 1] * B;
-      IP[i] = IP[i - 1] * IB;
-    }
-    return true;
-  }
-  int n;
-  vector<HH> h;                                                            // (normal, rev) hash
-  HH CH(char c, int i) { return {P[i] * (int)c, P[n - 1 - i] * (int)c}; }  // current hash value
+template <int MOD>
+class Modular {
  public:
-  Hashing(string s) {
-    if (!ISCALC) ISCALC = BasePowerCalculation();
-    n = (int)s.size();
-    h.resize(2 * n);
-    for (int i = n; i < 2 * n; ++i) h[i] = CH(s[i - n], i - n);
-    for (int i = n - 1; i > 0; --i) h[i] = h[i * 2] + h[i * 2 + 1];
+  static const int mod = MOD;
+  int v;
+  explicit operator int() const { return v; }
+  Modular() : v(0) {}
+  Modular(i64 _v) : v(int(_v % MOD)) { v += (v < 0) * MOD; }
+  Modular& operator+=(Modular o) {
+    if ((v += o.v) >= MOD) v -= MOD;
+    return *this;
   }
-  void update(int i, char c) {
-    for (h[i += n] = CH(c, i); i > 1; i /= 2) h[i / 2] = h[i] + h[i ^ 1];
+  Modular& operator-=(Modular o) {
+    if ((v -= o.v) < 0) v += MOD;
+    return *this;
   }
-  HH query(int l, int r) {
-    HH res = {(H){0, 0}, (H){0, 0}};
-    for (l += n, r += (n + 1); l < r; l /= 2, r /= 2) {
-      if (l & 1) res = res + h[l++];
-      if (r & 1) res = res + h[--r];
+  Modular& operator*=(Modular o) {
+    v = int(1LL * v * o.v % MOD);
+    return *this;
+  }
+  friend Modular power(Modular b, i64 p) {
+    Modular res = 1;
+    while (p > 0) {
+      if (p & 1) res *= b;
+      b *= b;
+      p >>= 1;
     }
     return res;
   }
-  H get_h(int l, int r) { return query(l, r)[0] * IP[l]; }
-  H get_rh(int l, int r) { return query(l, r)[1] * IP[n - 1 - r]; }
+  friend Modular inv(Modular b) {
+    assert(b.v != 0);
+    return power(b, MOD - 2);
+  }
+  friend Modular operator+(Modular a, Modular b) { return a += b; }
+  friend Modular operator-(Modular a, Modular b) { return a -= b; }
+  friend Modular operator*(Modular a, Modular b) { return a *= b; }
 };
+
+using mi = Modular<998244353>;
 
 void PreCalculation(void) {}
 
-// #define MultipleCase
+#define MultipleCase
 void Solve(int tc) {
-  int n, q;
-  cin >> n >> q;
-  string s;
-  cin >> s;
-  Hashing hs(s);
-  while (q--) {
-    int t;
-    cin >> t;
-    if (t == 1) {
-      int p;
-      char ch;
-      cin >> p >> ch;
-      hs.update(p - 1, ch);
-    } else {
-      int l, r;
-      cin >> l >> r;
-      --l, --r;
-      auto x = hs.get_h(l, r);
-      auto y = hs.get_rh(l, r);
-      if (x == y) {
-        cout << "YES\n";
-      } else {
-        cout << "NO\n";
-      }
+  int n;
+  cin >> n;
+  vector<int> a(n + 1);
+  vector<i64> f1(n + 1), f2(n + 1);
+  vector<mi> dp1(n + 1), dp2(n + 1);
+  for (int i = 1; i <= n; ++i) cin >> a[i];
+  f1[1] = max((i64)a[1], llabs(a[1]));
+  f2[1] = min((i64)a[1], llabs(a[1]));
+  if (a[1] >= 0) dp1[1] = dp2[1] = 2;
+  else dp1[1] = dp2[1] = 1;
+  for (int i = 2; i <= n; ++i) {
+    f1[i] = max({f1[i - 1] + a[i], llabs(f1[i - 1] + a[i]), f2[i - 1] + a[i], llabs(f2[i - 1] + a[i])});
+    if (f1[i - 1] != f2[i - 1]) {
+      if (f1[i] == f2[i - 1] + a[i]) dp1[i] += dp2[i - 1];
+      if (f1[i] == llabs(f2[i - 1] + a[i])) dp1[i] += dp2[i - 1];
     }
+    if (f1[i] == llabs(f1[i - 1] + a[i])) dp1[i] += dp1[i - 1];
+    if (f1[i] == f1[i - 1] + a[i]) dp1[i] += dp1[i - 1];
+
+    f2[i] = min({f1[i - 1] + a[i], llabs(f1[i - 1] + a[i]), f2[i - 1] + a[i], llabs(f2[i - 1] + a[i])});
+    if (f1[i - 1] != f2[i - 1]) {
+      if (f2[i] == f2[i - 1] + a[i]) dp2[i] += dp2[i - 1];
+      if (f2[i] == llabs(f2[i - 1] + a[i])) dp2[i] += dp2[i - 1];
+    }
+    if (f2[i] == llabs(f1[i - 1] + a[i])) dp2[i] += dp1[i - 1];
+    if (f2[i] == f1[i - 1] + a[i]) dp2[i] += dp1[i - 1];
   }
+  cout << (int) dp1[n] << '\n';
 }
 
 int main(void) {
@@ -126,4 +90,3 @@ int main(void) {
   }
   return 0;
 }
-// https://cses.fi/problemset/task/2420

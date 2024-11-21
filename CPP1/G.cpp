357118bd35d9ac9ac93568432e4e359739070483
TanbinHasan <tanvin.hasan74@gmail.com>
2024-11-22 01:27:20 +0600

    update

--- CPP1/G.cpp
+++ CPP1/G.cpp
@@ -1,11 +1,5 @@
 #include <bits/stdc++.h>
-#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
 using namespace std;
-using namespace __gnu_pbds;
-
-template<class T, class C = less<T>>
-using iset = tree<T, null_type, C, rb_tree_tag, tree_order_statistics_node_update>;
 
 typedef long long i64;
 
@@ -13,27 +7,7 @@ void PreCalculation(void) {}
 
 // #define MultipleCase
 void Solve(int tc) {
-  int n;
-  cin >> n;
-  vector<int> a(n + 1);
-  iset<int> si;
-  for (int i = 1; i <= n; ++i) {
-    cin >> a[i];
-    si.insert(i);
-  }
-  int prv = 1;
-  while ((int)si.size() > 1) {
-    int ind = si.order_of_key(prv);
-    
-    int rem = (a[prv] + ind) % (int)si.size();
-    rem = (rem - 1 + (int)si.size()) % (int)si.size();
-
-    prv = (rem + 1) % (int)si.size();
-    prv = *si.find_by_order(prv);
-    si.erase(si.find_by_order(rem));
-    // cout << prv << "\n";
-  }
-  cout << *(si).begin() << '\n';
+  
 }
 
 int main(void) {
@@ -49,15 +23,3 @@ int main(void) {
   }
   return 0;
 }
-/*
-1 3 4 5 6 7 8 9 10 
-1 4 5 6 7 8 9 10 
-1 5 6 7 8 9 10 
-1 6 7 8 9 10 
-1 7 8 9 10 
-1 8 9 10 
-1 9 10 
-1 10 
-10 
-10  
-*/
--- Python/1.py
+++ Python/1.py
@@ -1,36 +1,5 @@
-MOD = int(1e9 + 7)
-
-def fastadd(x, y):
-    return (MOD + (x % MOD) + (y % MOD)) % MOD
-
-def fastmul(x, y):
-    return ((x % MOD) * (y % MOD)) % MOD
-
-def binpow(x, y):
-    if y == 0:
-        return 1
-    powa = binpow(x, y // 2)
-    powa = fastmul(powa, powa)
-    if (y % 2 == 1):
-        return fastmul(powa, x)
-    return powa
-
-def modinv(x, m = MOD):
-    return binpow(x, m - 2)
-
-def main():
-    tc = int(input())
-    for _ in range(tc):
-        n = int(input())
-        arr = list(map(int, input().split()))
-
-        total_sum = sum(arr)
-
-        sol = 0
-        for i in range(n):
-            sol = fastadd(sol, fastmul(total_sum - arr[i], arr[i]))
-        
-        print(fastmul(sol, modinv(fastmul(n, n - 1))))
-
-if __name == "main__":
-    main()
+from math import pow
+for _ in range(int(input())):
+    a = float(input())
+    print("Case" , _+1 , end="")
+    print(":", round(pow(a/2, 3/2)/6,4))

206cba32078a0db1160c296203faed459e8ae5eb
TanbinHasan <tanvin.hasan74@gmail.com>
2025-06-23 22:28:04 +0600

    update

--- CPP1/A.cpp
+++ CPP1/A.cpp
@@ -7,22 +7,7 @@ void precalc(void) {}
 
 // #define testcase
 void solve(int tc) {
-  int n;
-  cin >> n;
-  vector<int> a(n);
-  map<int, vector<int>> ind;
-  for (int i = 0; i < n; ++i) {
-    cin >> a[i];
-    ind[a[i]].push_back(i);
-  }
-  int ans = numeric_limits<int>::max();
-  for (auto& [k, v] : ind) {
-    for (int i = 0; i + 1 < (int)v.size(); ++i) {
-      ans = min(ans, v[i + 1] - v[i] + 1);
-    }
-  }
-  if (ans == numeric_limits<int>::max()) ans = -1;
-  cout << ans << '\n';
+  
 }
 
 int main(void) {
